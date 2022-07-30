86fd11601f52991914a6d1babc3b8090cc54d33d
Matthew Heins <mkheins57@gmail.com>
2022-07-30 12:58:58 -0400

    Initial UI setup

--- YouTubeDownloaderApp/YouTubeDownloaderApp/MainActivity.cs
+++ YouTubeDownloaderApp/YouTubeDownloaderApp/MainActivity.cs
@@ -7,19 +7,31 @@ using AndroidX.AppCompat.Widget;
 using AndroidX.AppCompat.App;
 using Google.Android.Material.FloatingActionButton;
 using Google.Android.Material.Snackbar;
+using Android.Widget;
 
 namespace YouTubeDownloaderApp
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+
+        public FrameLayout FragmentContainer { get; set; }
+
+        public MainFragment MainFragment { get; set; }
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            SetContentView(Resource.Layout.activity_main);
+            SetContentView(Resource.Layout.MainActivity);
 
-            
+            FragmentContainer = FindViewById<FrameLayout>(Resource.Id.fragmentContainer);
+
+            MainFragment = new MainFragment();
+
+            SupportFragmentManager.BeginTransaction()
+                .Replace(Resource.Id.fragmentContainer, MainFragment)
+                .CommitAllowingStateLoss();
 
         }
 
--- YouTubeDownloaderApp/YouTubeDownloaderApp/MainFragment.cs
+++ YouTubeDownloaderApp/YouTubeDownloaderApp/MainFragment.cs
@@ -24,9 +24,11 @@ namespace YouTubeDownloaderApp
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             // Use this to return your custom view for this Fragment
-            // return inflater.Inflate(Resource.Layout.YourFragment, container, false);
+            var view = inflater.Inflate(Resource.Layout.MainFragment, container, false);
 
-            return base.OnCreateView(inflater, container, savedInstanceState);
+
+
+            return view;
         }
     }
 }
