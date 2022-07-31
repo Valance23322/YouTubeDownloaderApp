2b9fc80f850f53b9fbc0f1d1186fa99824afcc9a
ThePreparedProcrastinator <anthonyclips18@gmail.com>
2022-07-30 21:13:10 -0400

    added spinner options and listener/event handler for selecting website

--- YouTubeDownloaderApp/YouTubeDownloaderApp/MainFragment.cs
+++ YouTubeDownloaderApp/YouTubeDownloaderApp/MainFragment.cs
@@ -16,6 +16,7 @@ namespace YouTubeDownloaderApp
     {
 
         public Button DownloadBtn { get; set; }
+        public Spinner websiteSelectionSpinner { get; set; }
 
 
         public override void OnCreate(Bundle savedInstanceState)
@@ -29,14 +30,18 @@ namespace YouTubeDownloaderApp
         {
             // Use this to return your custom view for this Fragment
             var view = inflater.Inflate(Resource.Layout.MainFragment, container, false);
-
-
             
             DownloadBtn = view.FindViewById<Button>(Resource.Id.DownloadBtn);
             DownloadBtn.Click += ShowDownloadOptions;
 
+            websiteSelectionSpinner = view.FindViewById<Spinner>(Resource.Id.websiteSelection);
+            websiteSelectionSpinner.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs>(websiteSelected);
+
+            var adapter = ArrayAdapter.CreateFromResource(this.Context, Resource.Array.websiteSelectionOptions, Android.Resource.Layout.SimpleSpinnerItem);
+            adapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
+            websiteSelectionSpinner.Adapter = adapter;
+
             ParentFragmentManager.SetFragmentResultListener("DownloadOptions", ViewLifecycleOwner, new FragmentResultListener(OnFinishDownloadOptionsDialog));
-            SetupDialogAction();
             return view;
         }
 
@@ -44,6 +49,13 @@ namespace YouTubeDownloaderApp
         {
             var DownloadOptionsFragment = new DownloadOptionsFragment(DownloadAction);
             DownloadOptionsFragment.Show(ParentFragmentManager, "dialog");
+        }
+
+        public void websiteSelected (object sender, AdapterView.ItemSelectedEventArgs e)
+        {
+            Spinner websiteSelected = (Spinner)sender;
+            string toast = "You selected a website!";
+            Toast.MakeText(this.Context, toast, ToastLength.Short).Show();
         }
 
         public Action<string, string> DownloadAction { get; set; }
